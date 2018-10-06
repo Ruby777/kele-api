d150d5f7768c3ad007e93f535d0597d2d338dc97
Lucy <lucynn11@gmail.com>
2018-10-06 17:20:42 +0400

    irb-errors-roadmaps-and-checkpoints

--- lib/kele.rb
+++ lib/kele.rb
@@ -20,4 +20,14 @@ class Kele
         response = self.class.get('https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability', headers:{ "authorization" => @auth_token })
         JSON.parse(response.body)
     end
+
+    def get_roadmap(chain_id)
+        response = self.class.get('https://www.bloc.io/api/v1/roadmaps/#{chain_id}', headers:{ "authorization" => @auth_token })
+        JSON.parse(response.body)
+    end
+
+    def get_checkpoint(checkpoint_id)
+        response = self.class.get('https://www.bloc.io/api/v1/checkpoints/#{checkpoint_id}', headers:{ "authorization" => @auth_token })
+        JSON.parse(response.body)
+    end
 end
